78b4865e287b678b552bc42b2b0a9c89a654573a
niranjannlc <niranjannlc10@gmail.com>
2022-07-29 11:28:25 +0545

    understanding scope

--- app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
+++ app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
@@ -2,10 +2,18 @@ package org.lniranjan.coroutinepractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            delay(5000)
+            Log.d(" inside ","${Thread.currentThread().name}")
+        }
     }
 }
