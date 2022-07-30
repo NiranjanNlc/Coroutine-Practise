19cdb07c42c128017312340a83b845cf6a32c955
niranjannlc <niranjannlc10@gmail.com>
2022-07-30 12:01:31 +0545

    added the databinding in project

--- app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
+++ app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
@@ -3,12 +3,15 @@ package org.lniranjan.coroutinepractise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import kotlinx.coroutines.*
+import org.lniranjan.coroutinepractise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         GlobalScope.launch {
             delay(5000)
             Log.d(" inside ","${Thread.currentThread().name}")
