6b18c7f341a8031808345af7faca01aecfa60ca4
niranjannlc <niranjannlc10@gmail.com>
2022-07-30 11:56:02 +0545

    udestanding the concept of context

--- app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
+++ app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
@@ -3,9 +3,7 @@ package org.lniranjan.coroutinepractise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             Log.d(" inside ","$networkcall1")
             Log.d(" inside ","$networkcall2")
         }
+
+        //we can only do network call in the Io thread so
+        GlobalScope.launch(Dispatchers.IO) {
+            val value = doNetworkCall1()
+        //Ui is updated only form the main thread so we are switching the context
+            withContext(Dispatchers.Main)
+            {
+                //update  the Ui from here
+
+            }
+        }
+
     }
     suspend fun doNetworkCall1(): String {
         delay(3000)
