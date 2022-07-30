402a45d2b4eac43385ac1d3a5d42d87508b1da95
niranjannlc <niranjannlc10@gmail.com>
2022-07-30 12:05:45 +0545

    implemented the context

--- app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
+++ app/src/main/java/org/lniranjan/coroutinepractise/MainActivity.kt
@@ -14,22 +14,23 @@ class MainActivity : AppCompatActivity() {
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         GlobalScope.launch {
             delay(5000)
-            Log.d(" inside ","${Thread.currentThread().name}")
             val networkcall1 = doNetworkCall1()
             val networkcall2 = doNetworkCall2()
             // delays are added ip
             Log.d(" inside ","$networkcall1")
             Log.d(" inside ","$networkcall2")
         }
-
         //we can only do network call in the Io thread so
         GlobalScope.launch(Dispatchers.IO) {
             val value = doNetworkCall1()
-        //Ui is updated only form the main thread so we are switching the context
+            Log.d(" inside Io ","${Thread.currentThread().name}")
+
+            //Ui is updated only form the main thread so we are switching the context
             withContext(Dispatchers.Main)
             {
                 //update  the Ui from here
-
+                Log.d(" inside main ","${Thread.currentThread().name}")
+                binding.hellotext.text = value
             }
         }
 
@@ -40,6 +41,6 @@ class MainActivity : AppCompatActivity() {
     }
     suspend fun doNetworkCall2(): String {
         delay(3000)
-        return " network call 1"
+        return " network call 2"
     }
 }
