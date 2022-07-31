22d40faed79097064bf2e168488bbe093f2ac78b
niranjannlc <niranjannlc10@gmail.com>
2022-07-31 03:05:28 -0800

    adding dependancy

---
+++ app/src/main/java/org/lniranjan/coroutinepractise/LifeCycleViewModal.kt
@@ -0,0 +1,12 @@
+package org.lniranjan.coroutinepractise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LifeCycleViewModal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_life_cycle_view_modal)
+
+    }
+}
